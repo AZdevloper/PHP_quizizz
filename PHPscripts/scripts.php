1d7d2bc3eb0fc5a75f0d74db2ea581b3965e5a9c
ghali <ez.zahed.abdelaziz@gmail.com>
2023-01-04 09:21:44 +0100

    add score & username to data base

--- PHPscripts/scripts.php
+++ PHPscripts/scripts.php
@@ -1,42 +1,48 @@
 <?php
 require_once('../includes/autoloader.php');
-// if ($_GET['request'] == 1 ){
-//     // echo "tralalla tralalire";
-// }
+session_start();
+
 $conn1 = new DbConnection();
- # =========== get questions ========== #
- $query = $conn1->connect()->prepare("SELECT * FROM `questions` ;");
-$query->execute();
-$allquestion =  $query->fetchAll(PDO::FETCH_ASSOC);
- 
- $question = array();
- foreach($allquestion as $row){
-     $question[] = $row;
- }
+if (isset($_GET['score']) && isset($_GET['username'])) {
+    
+    $query = $conn1->connect()->prepare("INSERT INTO `user`( `username`, `score`) VALUES (?,?)");
+    $query->execute(array($_GET['username'],$_GET['score']));
+    echo 'scoore :'. $_GET['score'].'username :   '.$_GET['username'];
+    
+}else {
+    # code...
+    # =========== get questions ========== #
+    $query = $conn1->connect()->prepare("SELECT * FROM `questions` ;");
+   $query->execute();
+   $allquestion =  $query->fetchAll(PDO::FETCH_ASSOC);
+    
+    $question = array();
+    foreach($allquestion as $row){
+        $question[] = $row;
+    }
+   
+    # =========== get options ========== #
+    $query = $conn1->connect()->prepare("SELECT * FROM `choices` ;");
+    $query->execute();
+    $allquestion =  $query->fetchAll(PDO::FETCH_ASSOC);
+     
+    $choices = array();
+    foreach($allquestion as $row){
+        $choices[] = $row;
+    }
+   
+    # =========== insert options into questions ========== #
+    for($i=0; $i<sizeof($choices); $i++){
+        for($j=0; $j<sizeof($question); $j++){
+            if($choices[$i]['questionId'] == $question[$j]['id']){
+               $question[$j]['questionChoiceContent'][] = htmlspecialchars($choices[$i]['choiceContent']);
+               }
+        }
+    }
+   
+   echo json_encode($question) ;
 
- # =========== get options ========== #
- $query = $conn1->connect()->prepare("SELECT * FROM `choices` ;");
- $query->execute();
- $allquestion =  $query->fetchAll(PDO::FETCH_ASSOC);
-  
- $choices = array();
- foreach($allquestion as $row){
-     $choices[] = $row;
- }
-
- # =========== insert options into questions ========== #
- for($i=0; $i<sizeof($choices); $i++){
-     for($j=0; $j<sizeof($question); $j++){
-         if($choices[$i]['questionId'] == $question[$j]['id']){
-            $question[$j]['questionChoiceContent'][] = htmlspecialchars($choices[$i]['choiceContent']); 
-            
-            
-            }
-     }
- }
-
-echo json_encode($question) ;
-
+}   
 
 
 ?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -25,93 +25,68 @@ let get_random_number = (max, min) => {
   previousNumbers.push(randomNumber);
   return randomNumber;
 };
-// insert into counterlet
-// let x = 10;
-// let insert_into = ()=>{
-//    Timer_down.innerText = x;
-//    ++x;
-//   if (x < 0) {
-//       // clearInterval(intervale1);
-//       x = 10;
-//       show_next_quetion();
-//     }
-// }
-// count_Down
 
-// let time_for_each_question = 10000;
-
-// let intervale1 ;
-// let start_counter_down = ()=>{
-//    intervale1 = setInterval(() => {
-//     //  x=x-1;
-//    insert_into();
- 
-//   }, 1000);
-// }
-// for (let x = 0; x < 10; x++) {
-//  setTimeout(show_next_quetion,1000);
-  
-// }
-//timer
 var timerid;
 function timer() {
-  let count_down_duration = 2;
+  let count_down_duration =10;
   timerid = setInterval(() => {
-     Timer_down.innerText = count_down_duration;
-     --count_down_duration;
-     if (count_down_duration < 0) {
-       clearInterval(timerid);
-      //  show_next_quetion();
-     }
-   }, 2000);
- }
- // get data with ajax
- let questionContentData,idCorrectAnswerData,idData;
- let allData;
- function getData() {
+    Timer_down.innerText = count_down_duration;
+    --count_down_duration;
+    if (count_down_duration < 0) {
+      clearInterval(timerid);
+      show_next_quetion();
+    }
+  }, 1000);
+}
+// get data with ajax
+let allData;
+function getData() {
   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      // document.getElementById("potTextHer").innerHTML = this.responseText;
       let jsonResponse = JSON.parse(this.responseText);
-          allData = jsonResponse;
+      allData = jsonResponse;
     }
   };
-  let idCorrectAnswerData
-  let questionId = 1; 
-  xhttp.open("GET", `http://localhost/PHP_quizizz/PHPscripts/scripts.php?questionId=${questionId}?idCorrectAnswerData=${idCorrectAnswerData}`, false);
+
+  xhttp.open(
+    "GET",
+    `http://localhost/PHP_quizizz/PHPscripts/scripts.php?`,
+    false
+  );
   xhttp.send();
-  
 }
 getData();
 
 // show_next_quetion
 let number_of_questions = allData.length;
-let add_to_width = Math.floor( 100 / number_of_questions);
+let add_to_width = Math.floor(100 / number_of_questions);
 let width = 0;
 let current_question = 1;
 let current_answer;
-let progres_dis_withe = add_to_width; ; 
+let progres_dis_withe = add_to_width;
 let quetion_contenaire = getElement("quetion_contenaire");
 
 let show_next_quetion = () => {
-console.log("all data : ",allData);
+  console.log("all data : ", allData);
 
   if (current_question <= number_of_questions) {
     clearInterval(timerid);
-    // timer();
-   
-    let randomNumber =  get_random_number(number_of_questions-1, 0);
-    console.log("randomNumber :",randomNumber)
+    timer();
 
-    let {questionContent,idCorrectAnswer,questionChoiceContent} = allData[2];
+    let randomNumber = get_random_number(number_of_questions - 1, 0);
+    console.log("randomNumber :", randomNumber);
+
+    let { questionContent, idCorrectAnswer, questionChoiceContent } =
+      allData[randomNumber];
     question_id.innerText = questionContent;
-    answer1.innerText = questionChoiceContent[1].replace(/&lt;/g,'<');
-    answer2.innerText = questionChoiceContent[1].replace(/&lt;/g,'<');;
-    answer3.innerText = questionChoiceContent[2].replace(/&lt;/g,'<');;
-    answer4.innerText = questionChoiceContent[3].replace(/&lt;/g,'<');;
+    answer1.innerText = questionChoiceContent[0].replace(/&lt;/g, "<");
+    answer2.innerText = questionChoiceContent[1].replace(/&lt;/g, "<");
+    answer3.innerText = questionChoiceContent[2].replace(/&lt;/g, "<");
+    answer4.innerText = questionChoiceContent[3].replace(/&lt;/g, "<");
     current_answer = idCorrectAnswer;
+    console.log("idCorrectAnswer : ", idCorrectAnswer);
 
     let per_progresbar = getElement("per_progresbar");
     per_progresbar.innerText = progres_dis_withe + " %";
@@ -124,159 +99,132 @@ console.log("all data : ",allData);
       btn.classList.remove("background_green");
       btn.classList.remove("background_red");
     }
-    
   } else {
-     clearInterval(timerid);
+    clearInterval(timerid);
     let info_cyrcl2 = getElement("info_cyrcl2");
     info_cyrcl2.classList.add("steper_line_background_2");
     let info_cyrcl3 = getElement("info_cyrcl3");
-    info_cyrcl3.classList.add('steper_cyrcl_background');
+    info_cyrcl3.classList.add("steper_cyrcl_background");
     let resul_container = getElement("resul_container");
-  
+
     quetion_contenaire.style.display = "none";
     resul_container.classList.remove("d-none");
-    console.log(result);
 
     let div_logo = getElement("div_logo");
-    div_logo.innerText += Math.floor(result / number_of_questions) *100 +" %";
-    if ((result / number_of_questions) * 100 >= 50) {
-        let result_div = getElement("result_div");
-        // console.log(result_div);
-        result_div.insertAdjacentHTML(
-          "beforeend",
-          '<br><i class="happy_emoji">Congratulations! You passed the quiz.</i>'
-        );
-         clearInterval(intervale1);
+    let score = Math.floor(((result * 1) / number_of_questions) * 100) + " %";
+    div_logo.innerText +=score;
+    console.log("score :", score)
+    let username = inputName.value;
+      console.log("username :",username)
+      
+    $.get(`http://localhost/PHP_quizizz/PHPscripts/scripts.php?score=${score}&username=${username}`, function(data, status){
+    });
+      
 
-    }else {
-        let result_div = getElement("result_div");
-        console.log(result_div);
-        result_div.insertAdjacentHTML(
-          "beforeend",
-          '<i class="angry_emoji">Bad Result try again.</i>'
-        );
-      clearInterval(intervale1);
+    if ((result / number_of_questions) * 100 >= 50) {
+      let result_div = getElement("result_div");
+      result_div.insertAdjacentHTML(
+        "beforeend",
+        '<br><i class="happy_emoji">Congratulations! You passed the quiz.</i>'
+      );
+      clearInterval(timerid);
+    } else {
+      let result_div = getElement("result_div");
+      console.log(result_div);
+      result_div.insertAdjacentHTML(
+        "beforeend",
+        '<i class="angry_emoji">Bad Result try again.</i>'
+      );
+      clearInterval(timerid);
     }
-   
   }
 };
 
+// check answer function
+let clicked_btns = [];
+let result = 0;
+let check_answer = (chosen_btn_id, shosen_answer) => {
+  let chosen_btn = getElement(chosen_btn_id);
 
-
-// set intervale
-// let intervalId = null;
-// let isRunning = false;
-// let toggle = () => {
-//   if (isRunning) {
-//     clearInterval(intervalId);
-//     isRunning = false;
-//   } else {
-//     intervalId = setInterval(show_next_quetion, time_for_each_question);
-//     isRunning = true;
-//   }
-// };
-
-
-// check answer function 
-  let clicked_btns = [] ;
-  let result = 0;
-  let check_answer = (chosen_btn_id,shosen_answer)=>{
-    let chosen_btn = getElement(chosen_btn_id);
-      
-    if (shosen_answer == current_answer) {
-      result++;
-      chosen_btn.classList.add('background_green');
-      clicked_btns.push(chosen_btn);
-    }else { 
-      chosen_btn.classList.add("background_red");    
-      clicked_btns.push(chosen_btn);
-    }
-
+  if (shosen_answer == current_answer) {
+    result++;
+    chosen_btn.classList.add("background_green");
+    clicked_btns.push(chosen_btn);
+  } else {
+    chosen_btn.classList.add("background_red");
+    clicked_btns.push(chosen_btn);
   }
+};
 // setInterval(intervaleid);
 let container = getElement("answers_container");
 let chosen_btn_id;
 container.addEventListener("click", (event) => {
   if (event.target.id === "answer1") {
-    
-  //  let shosen_answer = 1;
-  //  let chosen_btn_id = "answer1";
-    check_answer("answer1",1);
-    setTimeout(show_next_quetion, 1000); 
+    check_answer("answer1", 1);
+    setTimeout(show_next_quetion, 1000);
     clearInterval(timerid);
-    timer(); 
-    // toggle();
-    // toggle();
-  } else if (event.target.id === "answer2") {
-    check_answer("answer2",2);
-    setTimeout(show_next_quetion, 1000); 
-     clearInterval(timerid);
-     timer(); 
-    // toggle();
-    // toggle();
-  } else if (event.target.id === "answer3") {
-    check_answer("answer3",3);
-    setTimeout(show_next_quetion, 1000); 
-     clearInterval(timerid);
-     timer(); 
-    // toggle();
-    // toggle();
-  } else if (event.target.id === "answer4") {
-    check_answer("answer4",4);
-    setTimeout(show_next_quetion, 1000); 
-     clearInterval(timerid);
-     timer(); 
-    // toggle();
-    // toggle();
-  } 
+    timer();
 
+  } else if (event.target.id === "answer2") {
+    check_answer("answer2", 2);
+    setTimeout(show_next_quetion, 1000);
+    clearInterval(timerid);
+    timer();
+
+  } else if (event.target.id === "answer3") {
+    check_answer("answer3", 3);
+    setTimeout(show_next_quetion, 1000);
+    clearInterval(timerid);
+    timer();
+
+  } else if (event.target.id === "answer4") {
+    check_answer("answer4", 4);
+    setTimeout(show_next_quetion, 1000);
+    clearInterval(timerid);
+    timer();
+
+  }
 });
 
-// submitbtn.addEventListener("click",(avent)=>{
-//  clicked_btns.free();
-// }
 
-// )
 let info_cyrcl1 = getElement("info_cyrcl1");
-info_cyrcl1.classList.add('steper_cyrcl_background');
+info_cyrcl1.classList.add("steper_cyrcl_background");
 
 
+let start_quiz = () => {
+  counter.classList.add("d-none");
+  info_cyrcl1.classList.add("steper_line_background_1");
+  let info_cyrcl2 = getElement("info_cyrcl2");
+  info_cyrcl2.classList.add("steper_cyrcl_background");
 
-btnstart.addEventListener("click",()=>{
-  let start_quiz = ()=>{
-    counter.classList.add("d-none");
-    info_cyrcl1.classList.add('steper_line_background_1');
-    let info_cyrcl2 = getElement("info_cyrcl2");
-    info_cyrcl2.classList.add("steper_cyrcl_background");
+  home.classList.add("d-none");
+  quetion_contenaire.classList.remove("d-none");
 
-    home.classList.add('d-none');
-    quetion_contenaire.classList.remove("d-none");
-
-     
-     show_next_quetion(); 
+  show_next_quetion();
+};
+// start_quiz();
+btnstart.addEventListener("click", () => {
+  if (inputName.value == "") {
+    $("#inputName").addClass("inputNameNull");
+  } else {
+    let counter = getElement("counter");
+    counter.classList.remove("d-none");
+    let counter_p = getElement("counter_p");
+    let x =3;
+    let intervale =  setInterval(() => {
+      counter_p.innerText = x;
+      x--;
+      if(x<-1) {
+        clearInterval(intervale);
+        start_quiz();
+      }
+    }, 1000);
   }
-  start_quiz();
-  // //  timer   3 to 0
-      // let counter = getElement("counter");
-      // counter.classList.remove("d-none");
-      // let counter_p = getElement("counter_p");
-      // let x =3;
-      // let intervale =  setInterval(() => {
-      //   counter_p.innerText = x;
-      //   x--;
-      //   if(x<-1) {
-      //     clearInterval(intervale);
-      //     start_quiz();
-      //   }
-      // }, 1000);
-  
-// fetch('Classes/dbConnection.class.php')
-// .then((respons)=>console.log()).then((data)=>{
-//   console.log(data);
-
-// })
 
 
-  
-  
+
+  // fetch('Classes/dbConnection.class.php')
+  // .then((respons)=>console.log()).then((data)=>{
+  //   console.log(data);
+
 });
